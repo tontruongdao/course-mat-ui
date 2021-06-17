7d26fb1b80c2b7cdccb845e457e2a0f5c32ef375
truongsinistar <truong@sinistar.ca>
2021-06-17 13:41:31 -0400

    Mat-UI icon

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import  Typography  from '@material-ui/core/Typography'
+import Typography  from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Container from '@material-ui/core/Container'
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined'
+import SendIcon from '@material-ui/icons/Send'
 
 export default function Create() {
   return (
@@ -19,21 +21,35 @@ export default function Create() {
       >
         Create a new note
       </Typography> */}
+      {/* Button */}
       <Button 
         type="submit"
         color="secondary"
         variant="outlined"
+        startIcon={<SendIcon/>}
       >
         Submit
-      </Button>
-      <ButtonGroup color="secondary" variant="contained">
+      </Button> 
+
+      {/* <ButtonGroup color="secondary" variant="contained">
         <Button>One</Button>
         <Button>Two</Button>
         <Button>Three</Button>
       </ButtonGroup>
       <Button type="submit" color="secondary" variant="contained" onClick={() => console.log("you clicked me")}>
         Submit
-      </Button>
+      </Button> */}
+      {/* icons */}
+      <br/>
+      <AcUnitOutlinedIcon/>
+      <AcUnitOutlinedIcon 
+        color="secondary"
+        fontSize="large"
+      />
+      <AcUnitOutlinedIcon 
+        color="action"
+        fontSize="small"
+      />
     </Container>
   )
 }
